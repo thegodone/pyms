b902ac30533d40ad01ae259985fca0897d63f234
milica.ng14 <milica.ng14@7fdfcb43-353e-0410-af7f-dbb92ee9c758>
2008-05-05 07:54:45 +0000

    'META-B' changed to 'PyMS' Copyright date changed

--- trunk/Utils/Error.py
+++ trunk/Utils/Error.py
@@ -5,8 +5,8 @@
 
  #############################################################################
  #                                                                           #
- #    META-B software for processing of metabolomic mass-spectrometry data   #
- #    Copyright (C) 2005-6 Vladimir Likic                                    #
+ #    PyMS software for processing of metabolomic mass-spectrometry data     #
+ #    Copyright (C) 2005-8 Vladimir Likic                                    #
  #                                                                           #
  #    This program is free software; you can redistribute it and/or modify   #
  #    it under the terms of the GNU General Public License version 2 as      #
