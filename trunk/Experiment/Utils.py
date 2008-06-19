c562ebf159524b9b4e3936cb520fca0edf28d5dd
v.likic <v.likic@7fdfcb43-353e-0410-af7f-dbb92ee9c758>
2008-06-19 07:13:13 +0000

    Added Experiment/Utils.py

---
+++ trunk/Experiment/Utils.py
@@ -0,0 +1,36 @@
+"""
+Utilities for the subpackage Experiment
+"""
+
+ #############################################################################
+ #                                                                           #
+ #    PyMS software for processing of metabolomic mass-spectrometry data     #
+ #    Copyright (C) 2005-8 Vladimir Likic                                    #
+ #                                                                           #
+ #    This program is free software; you can redistribute it and/or modify   #
+ #    it under the terms of the GNU General Public License version 2 as      #
+ #    published by the Free Software Foundation.                             #
+ #                                                                           #
+ #    This program is distributed in the hope that it will be useful,        #
+ #    but WITHOUT ANY WARRANTY; without even the implied warranty of         #
+ #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          #
+ #    GNU General Public License for more details.                           #
+ #                                                                           #
+ #    You should have received a copy of the GNU General Public License      #
+ #    along with this program; if not, write to the Free Software            #
+ #    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.              #
+ #                                                                           #
+ #############################################################################
+
+def cmp_peak_area(peak1, peak2):
+
+    """cmp_peak_area(peak1, peak2)
+
+    Compares peak normalised area.
+
+    @param peak1 A peak object.
+    @param peak2 A peak object.
+    """
+
+    return cmp(peak1.norm_area, peak2.norm_area)
+
