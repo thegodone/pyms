cf6f8568155ce7f866ce6d8f3277df5341088a2c
v.likic <v.likic@7fdfcb43-353e-0410-af7f-dbb92ee9c758>
2009-02-18 02:56:56 +0000

    Added new Function.py, Qiao to take over for fixing (see previous commit)

---
+++ trunk/libMS/Function.py
@@ -0,0 +1,86 @@
+"""
+General functions for library matching 
+"""
+
+ #############################################################################
+ #                                                                           #
+ #    PyMS software for processing of metabolomic mass-spectrometry data     #
+ #    Copyright (C) 2005-8 Vladimir Likic                                    #
+ #                                                                           #
+ #    This program is free software; you can redistribute it and/or modify   #
+ #    it under the terms of the GNU General Public License version 2 as      #
+ #    published by the Free Software Foundation.                             #
+ #                                                                           #
+ #    This program is distributed in the hope that it will be useful,        #
+ #    but WITHOUT ANY WARRANTY; without even the implied warranty of         #
+ #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          #
+ #    GNU General Public License for more details.                           #
+ #                                                                           #
+ #    You should have received a copy of the GNU General Public License      #
+ #    along with this program; if not, write to the Free Software            #
+ #    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.              #
+ #                                                                           #
+ #############################################################################
+
+import numpy
+
+from pyms.Utils.Error import error
+
+def ms_lib_match(ms_lib, ms):
+
+    """
+    @summary: Matches a mass spectrum the records in the mass spectral
+        library
+
+    @para ms_lib: Mass spectral library
+    @type ms_lib: pyms.libMS.JCAMP.Class.MSLib
+    @para ms: The query mass spectrum
+    @type ms: pyms.IO.Class.MassSpectrum
+
+    @return: The compound name, similarity value, and mass spectrum
+    @rtype: ListType
+
+    @author: Qiao Wang
+    @author: Vladimir Likic
+    """
+
+    # for each record, set the attribute 'mass_spectrum'
+    max = 0.0
+    compound = None
+    mass = None 
+
+    for record in ms_lib.records:
+
+        if len(record.mass_record) == 0:
+            error("library mass record with no mass spectrum")
+
+        score = 0.0
+
+        array1 = []
+        array2 = []
+
+        for item in record.mass_record:
+            if item[0] >= ms.mz[0] and item[0] <= ms.mz[len(ms.mz) - 1]:
+                array1.append(item[1])
+                array2.append(im[mass_list.index(item[0])])
+
+        mass_spect1 = numpy.array(array1, dtype='d')
+        mass_spect2 = numpy.array(array2, dtype='d')
+
+        mass_spect1_sum = numpy.sum(mass_spect1 ** 2, axis=0)
+        mass_spect2_sum = numpy.sum(mass_spect2 ** 2, axis=0)
+
+        top = numpy.dot(mass_spect1, mass_spect2)
+        bot = numpy.sqrt(mass_spect1_sum * mass_spect2_sum)
+        cos = top / bot
+        score = 1.0 - cos
+
+        if score > max:
+            max = score
+            compound = record.name
+            mass = record.mass_record
+
+    result = [compound, max, mass]
+
+    return result
+
