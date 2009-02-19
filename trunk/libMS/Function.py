7482e4ee0e8e5d7f1e30d4d0deea5537c27199d2
v.likic <v.likic@7fdfcb43-353e-0410-af7f-dbb92ee9c758>
2009-02-19 06:02:54 +0000

    Reworked ms_lib_match()

--- trunk/libMS/Function.py
+++ trunk/libMS/Function.py
@@ -26,7 +26,7 @@ import numpy
 
 from pyms.Utils.Error import error
 
-def ms_lib_match(ms_lib, ms):
+def ms_lib_match2(ms_lib, ms):
 
     """
     @summary: Matches mass spectrum 'ms' against all the records
@@ -42,33 +42,80 @@ def ms_lib_match(ms_lib, ms):
     @author: Vladimir Likic
     """
 
-    # for each record, set the attribute 'mass_spectrum'
-    max = 0.0
-    compound = ''
+    # initialise best match attributes
+    best_score = 0.0
+    compound = None
     mass = []
-    mass_list = ms.mass_list
-    im = ms.mass_spec
+
+    # find min/max mz in the query mass spectrum
+    min_mz = min(ms.mass_list)
+    max_mz = max(ms.mass_list)
+
+    # fill in the the mz_index dictionary. Keys are mz values,
+    # values are indices in the intensity list
+    mz_index = {}
+    for ii in range(len(ms.mass_list)):
+        mz = ms.mass_list[ii]
+        mz_index[mz] = ii
+
+    # loop over all records in the library
     for record in ms_lib.records:
-        score = 0.0
-        if len(record.mass_record) > 0:
-            array1 = im
-            array2 = []
-            for i in range(len(array1)):
-                array2.append(0)
-            for item in record.mass_record:
-                if item[0] >= mass_list[0] and item[0] <= mass_list[len(mass_list) - 1]:
-                    array2[item[0] - 50] = item[1]
-            mass_spect1 = numpy.array(array1, dtype='d')
-            mass_spect2 = numpy.array(array2, dtype='d')
-            mass_spect1_sum = numpy.sum(mass_spect1 ** 2, axis=0)
-            mass_spect2_sum = numpy.sum(mass_spect2 ** 2, axis=0)
-            top = numpy.dot(mass_spect1, mass_spect2)
-            bot = numpy.sqrt(mass_spect1_sum * mass_spect2_sum)
-            cos = top / bot
-            score = cos
-        if score > max:
-            max = score
+
+        record_score = 0.0
+
+        # bomb-out if record has an empty mass spectrum
+        if len(record.mass_record) == 0:
+            error("library record with empty mass spectrum")
+
+        # initialize record mass spectrum
+        record_mass_spec = []
+        for i in range(len(ms.mass_list)):
+            record_mass_spec.append(0)
+
+        # fill in record mass spectrum
+        for item in record.mass_record:
+
+            record_mz = item[0]
+            intensity = item[1]
+
+            # consider only record mz values that are in the range
+            # of the query mass spectrum
+            if (record_mz >= min_mz) and (record_mz <= max_mz):
+
+                # query mass spectrum must have this mz
+                if not record_mz in mz_index.keys():
+                    error("mz value missing in the library record")
+                else: # assign intensity to the record mz value
+                    ii = mz_index[record_mz]
+                    record_mass_spec[ii] = intensity
+
+        # get the mass spectral similarity score
+        record_score = costheta(ms.mass_spec, record_mass_spec)
+
+        # consider if the best hit
+        if record_score > best_score:
+            best_score = record_score
             compound = record.name
             mass = record.mass_record
-    result = [compound, max, mass]
+
+    result = [compound, best_score, mass]
+
     return result
+
+def costheta(v1, v2):
+
+    """
+    """
+
+    mass_spect1 = numpy.array(v1, dtype='d')
+    mass_spect2 = numpy.array(v2, dtype='d')
+    mass_spect1_sum = numpy.sum(mass_spect1 ** 2, axis=0)
+    mass_spect2_sum = numpy.sum(mass_spect2 ** 2, axis=0)
+
+    top = numpy.dot(mass_spect1, mass_spect2)
+    bot = numpy.sqrt(mass_spect1_sum * mass_spect2_sum)
+    cos = top / bot
+
+    return cos
+
+
